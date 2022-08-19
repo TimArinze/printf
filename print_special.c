e61cf00a3a40b83f8915354787495c157c5819c4
Marvins14 <nkoromacilinus@gmail.com>
2022-08-19 11:27:31 -0700

    add

---
+++ print_special.c
@@ -0,0 +1,21 @@
+pecials - prints special character
+ * @next: character after the %
+ *  * @arg: argument for the identifer
+ *   * Return: the number of characters printed
+ *    */
+ int print_specials(char next, va_list arg)
+{
+	        int index;
+
+		        specialsStruct functs[] = {
+				                {"c", print_char},
+						                {"s", print_str},
+								                {NULL, NULL}
+						        };
+			        for (index = 0; functs[index].indentifier != NULL; index++)
+					        {
+							                if (functs[index].indentifier[0] == next)
+										                        return (functs[index].printer(arg));
+									        }
+				        return (0);
+}
