77d5bcf96ccdd30c960920cd318f39aeaa428d31
Arinze Nwutobo <tim.arinze@outlook.com>
2022-08-19 17:41:10 +0000

    task

--- main.h
+++ main.h
@@ -1,1 +1,22 @@
+#ifndef MAIN_H
 
+#define MAIN_H
+
+#include <stdarg.h>
+
+int _putchar(char c);
+
+
+/**
+ * struct specialsStruct - structure
+ * definition of a printTypeStruct
+ * @indentifier: type
+ * @printer: funtion to print
+ */
+typedef struct specialsStruct
+{
+	char *indentifier;
+	int (*printer)(va_list);
+} specialsStruct;
+
+#endif
